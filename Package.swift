f55559853a01e2d836b742709b75d3d38ce3fc56
ainame <s.namai.09@gmail.com>
2026-02-15 20:19:05 +0000

    Rename package identity to swift-migemo

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Migemo",
+    name: "swift-migemo",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
